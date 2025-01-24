2955be6c5c7c0df4dbc1340f88bff01449707305
Gerardo Martin <gerardomartin_03@hotmail.com>
2025-01-24 00:42:58 -0600

    Refactor single number detection methods to use functions for XOR and count approaches

--- singleNumber.py
+++ singleNumber.py
@@ -2,12 +2,21 @@ import operator
 numbers = [2,2,1]
 
 #time complexity = O(n) using xor
-tr = 0
-for num in numbers:
-    tr = operator.xor(tr, num)
-print(tr)
+def singleNumberXor(numbers: list[int])->int:
+    tr = 0
+    for num in numbers:
+        tr = operator.xor(tr, num)
+        if tr is None:
+            return 0
+    return tr
+print(singleNumberXor(numbers))
 
 #time complexity = O(n^2) using count
-for i in numbers:
-    if numbers.count(i) == 1:
-        print(i)
+def singleNumberCount(numbers: list[int])->int:
+    for i in numbers:
+        if numbers.count(i) == 1:
+            continue
+        return i
+
+print(singleNumberCount(numbers))
+        
